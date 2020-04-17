57aa49d0bf79f347e179616fc3ca0c0d9bb46b52
TortoiseTiago <tortoise.tl@gmail.com>
2020-04-17 13:02:58 -0300

    delete

--- public/script.js
+++ public/script.js
@@ -1,5 +1,7 @@
 const currentPage = location.pathname
 const menuItens = document.querySelectorAll("header .links a")
+const pagination = document.querySelector(".pagination")
+
 
 for (item of menuItens) {
     if (currentPage.includes(item.getAttribute("href"))) {
@@ -9,14 +11,55 @@ for (item of menuItens) {
     // ao caminho da rota e adiciona a classe active ao CSS
 }
 
+if (pagination) {
+    createPagination(pagination)
+}
 
-const formDelete = document.querySelector("#formDelete")
+function paginate(selectedPage, totalPages) {
 
-formDelete.addEventListener("submit", function(e) {
-    const confirmation = confirm("Deseja deletar?")
+    let pages = [],
+        oldPage
 
-    if (!confirmation) {
-        e.preventDefault()
+    for (let currentPage = 1; currentPage <= totalPages; currentPage++) {
+
+        const FirstAndLastPage = currentPage == 1 || currentPage == totalPages
+        const pagesAfter = currentPage <= selectedPage + 2
+        const pagesBefore = currentPage >= selectedPage - 2
+
+        if (FirstAndLastPage || pagesBefore && pagesAfter) {
+            if (oldPage && currentPage - oldPage > 2) {
+                pages.push("...")
+            }
+
+            if (oldPage && currentPage - oldPage == 2) {
+                pages.push(oldPage + 1)
+            }
+
+            pages.push(currentPage)
+            oldPage = currentPage
+        }
     }
+    return pages
+}
 
-})
+function createPagination(pagination) {
+    const filter = pagination.dataset.filter
+    const page = +pagination.dataset.page
+    const total = +pagination.dataset.total
+    const pages = paginate(page, total)
+    
+    let elements = ""
+    
+    for (let page of pages) {
+        if (String(page).includes("...")) {
+            elements += `<span>${page}</span>`
+        } else {
+            if (filter) {
+                elements += `<a href="?page=${page}&filter=${filter}">${page}</a>`
+            } else {
+                elements += `<a href="?page=${page}">${page}</a>`
+            }
+        }
+    }
+    pagination.innerHTML = elements
+}
