9fc9e8f3c7b1c0ce47b0c3371c6ad7b5be6e893a
TortoiseTiago <tortoise.tl@gmail.com>
2021-02-22 00:06:59 -0300

    bug express/banco de dados corrigido

--- src/app/models/Member.js
+++ src/app/models/Member.js
@@ -1,19 +1,22 @@
-const { date } = require("../../lib/utils")
+const { date } = require("../../lib/utils");
 
-const db = require("../../config/db")
+const db = require("../../config/db");
 
 module.exports = {
-    all(callback) {
-        db.query(`SELECT * 
+  all(callback) {
+    db.query(
+      `SELECT * 
         FROM members 
-        ORDER BY name ASC`, function (err, results) {
-            if (err) throw `Database error! ${err}`
+        ORDER BY name ASC`,
+      function (err, results) {
+        if (err) throw `Database error! ${err}`;
 
-            callback(results.rows)
-        })
-    },
-    create(data, callback) {
-        const query = `
+        callback(results.rows);
+      }
+    );
+  },
+  create(data, callback) {
+    const query = `
             INSERT INTO members (
                 avatar_url,
                 name,
@@ -26,38 +29,42 @@ module.exports = {
                 instructor_id
             ) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9)
             RETURNING id
-        `
-        const values = [
-            data.avatar_url,
-            data.name,
-            data.email,
-            data.gender,
-            date(data.birth).iso,
-            data.blood,
-            data.weight,
-            data.height,
-            data.instructor
-        ]
+        `;
+    const values = [
+      data.avatar_url,
+      data.name,
+      data.email,
+      data.gender,
+      date(data.birth).iso,
+      data.blood,
+      data.weight,
+      data.height,
+      data.instructor,
+    ];
 
-        db.query(query, values, function (err, results) {
-            if (err) throw `Database error! ${err}`
+    db.query(query, values, function (err, results) {
+      if (err) throw `Database error! ${err}`;
 
-            callback(results.rows[0])
-        })
-    },
-    find(id, callback) {
-        db.query(`
+      callback(results.rows[0]);
+    });
+  },
+  find(id, callback) {
+    db.query(
+      `
             SELECT members.*, instructors.name AS instructor_name
             FROM members 
             LEFT JOIN instructors ON (members.instructor_id = instructors.id)
-            WHERE members.id = $1`, [id], function (err, results) {
-            if (err) throw `Database error! ${err}`
+            WHERE members.id = $1`,
+      [id],
+      function (err, results) {
+        if (err) throw `Database error! ${err}`;
 
-            callback(results.rows[0])
-        })
-    },
-    update(data, callback) {
-        const query = `
+        callback(results.rows[0]);
+      }
+    );
+  },
+  update(data, callback) {
+    const query = `
             UPDATE members SET 
             avatar_url=($1),
             name=($2),
@@ -69,74 +76,79 @@ module.exports = {
             height=($8),
             instructor_id=($9)
         WHERE id = $10
-        `
-        const values = [
-            data.avatar_url,
-            data.name,
-            data.email,
-            data.gender,
-            date(data.birth).iso,
-            data.blood,
-            data.weight,
-            data.height,
-            data.instructor,
-            data.id
-        ]
+        `;
+    const values = [
+      data.avatar_url,
+      data.name,
+      data.email,
+      data.gender,
+      date(data.birth).iso,
+      data.blood,
+      data.weight,
+      data.height,
+      data.instructor,
+      data.id,
+    ];
 
-        db.query(query, values, function (err, results) {
-            if (err) throw `Database error! ${err}`
+    db.query(query, values, function (err, results) {
+      if (err) throw `Database error! ${err}`;
 
-            callback()
-        })
-    },
-    delete(id, callback) {
-        db.query(`
-            DELETE FROM members WHERE id = $1`, [id], function (err, results) {
-            if (err) throw `Database error! ${err}`
+      callback();
+    });
+  },
+  delete(id, callback) {
+    db.query(
+      `
+            DELETE FROM members WHERE id = $1`,
+      [id],
+      function (err, results) {
+        if (err) throw `Database error! ${err}`;
 
-            callback()
-        })
-    },
-    instructorsSelectOptions(callback) {
-        db.query(`
-        SELECT name, id FROM instructors`, function (err, results) {
-            if (err) throw `Database error! ${err}`
+        callback();
+      }
+    );
+  },
+  instructorsSelectOptions(callback) {
+    db.query(
+      `
+        SELECT name, id FROM instructors`,
+      function (err, results) {
+        if (err) throw `Database error! ${err}`;
 
-            callback(results.rows)
-        })
-    },
-    paginate(params) {
+        callback(results.rows);
+      }
+    );
+  },
+  paginate(params) {
+    const { filter, limit, offset, callback } = params;
 
-        const { filter, limit, offset, callback } = params
-
-        let query = "",
-            filterQuery = "",
-            totalQuery = `(
+    let query = "",
+      filterQuery = "",
+      totalQuery = `(
                 SELECT count(*) FROM members
-            ) AS total `
+            ) AS total `;
 
-        if (filter) {
-
-            filterQuery = `
+    if (filter) {
+      filterQuery = `
             WHERE members.name ILIKE '%${filter}%'
             OR members.email ILIKE '%${filter}%'
-            `
-            totalQuery = `(
+            `;
+      totalQuery = `(
                 SELECT count(*) FROM members
                 ${filterQuery}
-            ) AS total`
-        }
+            ) AS total`;
+    }
 
-        query = `
+    query = `
         SELECT members.*, ${totalQuery}
         FROM members
         ${filterQuery}
         LIMIT $1 OFFSET $2
-        `
-        db.query(query, [limit, offset], function (err, results) {
-            if (err) throw `Database error! ${err}`
+        `;
+    db.query(query, [limit, offset], function (err, results) {
+      if (err) throw `Database error! ${err}`;
 
-            callback(results.rows)
-        })
-    }
-}
+      callback(results.rows);
+    });
+  },
+};
--- src/config/db.js
+++ src/config/db.js
@@ -1,9 +1,9 @@
-const { Pool } = require('pg')
+const { Pool } = require("pg");
 
 module.exports = new Pool({
-    user: "postgres",
-    password:"LOLtbl.2014",
-    host: "localhost",
-    port: 5432,
-    database:"gymmanager"
-})
+  user: "tiago",
+  password: "LOLtbl.2014",
+  host: "localhost",
+  port: 5432,
+  database: "gym-manager",
+});
